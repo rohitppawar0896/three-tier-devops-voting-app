9ed032cac55ec9a6c109e605812e6f0316704767
Rohit Pawar <rohitppawar0896@gmail.com>
2026-06-04 22:49:02 +0530

    feat: change voting options to cricket and football

--- app/vote/app.py
+++ app/vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Cricket")
+option_b = os.getenv('OPTION_B', "Football")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
@@ -16,12 +16,14 @@ gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.INFO)
 
+
 def get_redis():
     if not hasattr(g, 'redis'):
         g.redis = Redis(host="redis", db=0, socket_timeout=5)
     return g.redis
 
-@app.route("/", methods=['POST','GET'])
+
+@app.route("/", methods=['POST', 'GET'])
 def hello():
     voter_id = request.cookies.get('voter_id')
     if not voter_id:
